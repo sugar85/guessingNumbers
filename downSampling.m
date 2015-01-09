4ca609d3707fc51e49378ba62b7197cf8e1cda06
duongnguyen <duongnguyen@kms-technology.com>
2015-01-09 18:27:38 +0700

    updating

--- downSampling.m
+++ downSampling.m
@@ -1,4 +1,4 @@
-function J = downSampling(I)
+function K = downSampling(I)
 %low_pass_filter = fspecial('average',[3,3]);
 low_pass_filter = fspecial('gaussian',[3,3]);
 J = imfilter(I,low_pass_filter,'replicate');
@@ -13,17 +13,18 @@ J(:,j*2)=[];
 j=j-1;
 end;
 [m n]=size(J);
-min = min(min(J));
-max = max(max(J));
+%min = min(min(J));
+%max = max(max(J));
 for i=1:m
 	for j=1:n
 		% normalize
-		J(i,j) = (J(i,j) - min)/(max - min);
-		%if(J(i,j)>=0.1 && J(i,j)<0.5)
-		%	J(i,j) = J(i,j) + 0.5;
-		%elseif (J(i,j) < 0.1)
-		%	J(i,j) = 0;
-		%end;
+		%J(i,j) = (J(i,j) - min)/(max - min);
+		if(J(i,j)>=0.1 && J(i,j)<0.5)
+			J(i,j) = J(i,j) + 0.5;
+		elseif (J(i,j) < 0.1)
+			J(i,j) = 0;
+		end;
 	end;
 end;
+K = J;
 end
--- test.m
+++ test.m
@@ -2,14 +2,14 @@ pkg load image;
 load('trainedWeight-0.01.mat');
 
 X = processingImage('testData/zero.bmp');
-X = [X;processingImage('testData/1.bmp')];
-X = [X;processingImage('testData/2.bmp')];
-X = [X;processingImage('testData/3.bmp')];
-X = [X;processingImage('testData/4.bmp')];
-X = [X;processingImage('testData/5.bmp')];
-X = [X;processingImage('testData/six.jpg')];
-X = [X;processingImage('testData/7.bmp')];
-X = [X;processingImage('testData/8.bmp')];
-X = [X;processingImage('testData/9.bmp')];
+%X = [X;processingImage('testData/1.bmp')];
+%X = [X;processingImage('testData/2.bmp')];
+%X = [X;processingImage('testData/3.bmp')];
+%X = [X;processingImage('testData/4.bmp')];
+%X = [X;processingImage('testData/5.bmp')];
+%X = [X;processingImage('testData/6.bmp')];
+%X = [X;processingImage('testData/7.bmp')];
+%X = [X;processingImage('testData/8.bmp')];
+%X = [X;processingImage('testData/9.bmp')];
 y = predict(Theta1,Theta2,X);
 y
